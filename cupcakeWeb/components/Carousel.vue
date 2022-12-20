dcbfe56755db4e6a77e331cec5251da5886bfa22
Maryam_rshn <mari.r9777@gmail.com>
2022-12-20 14:22:56 +0330

    carousel component and carousel section added and styled

---
+++ cupcakeWeb/components/Carousel.vue
@@ -0,0 +1,228 @@
+<template>
+    <div class="carousel" ref="carousel">
+      <div class="slider-wrapper">
+        <div class="inner" ref="inner">
+          <slot></slot>
+        </div>
+      </div>
+      <div class="carousel-nav">
+        <button class="prev" @click="switchToPrev()">
+          <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m13.789 7.155c.141-.108.3-.157.456-.157.389 0 .755.306.755.749v8.501c0 .445-.367.75-.755.75-.157 0-.316-.05-.457-.159-1.554-1.203-4.199-3.252-5.498-4.258-.184-.142-.29-.36-.29-.592 0-.23.107-.449.291-.591 1.299-1.002 3.945-3.044 5.498-4.243z"/></svg>
+        </button>
+        <button class="next" @click="switchToNext()">
+          <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m10.211 7.155c-.141-.108-.3-.157-.456-.157-.389 0-.755.306-.755.749v8.501c0 .445.367.75.755.75.157 0 .316-.05.457-.159 1.554-1.203 4.199-3.252 5.498-4.258.184-.142.29-.36.29-.592 0-.23-.107-.449-.291-.591-1.299-1.002-3.945-3.044-5.498-4.243z"/></svg>
+        </button>
+      </div>
+      <div class="carousel-dots-wrapper" ref="dotsWrapper">
+        <button v-for='index in this.numberOfDots' :key='index' :class="[ 'carousel-dot', `step-${index}` ]" :step="index" @click='switchToStep(index)'></button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+      name: "Carousel",
+      props: {
+        optionsObject: Object,
+      },
+      
+      data() {
+        return {
+          innerWidth: 0,
+          slideWidth: 0,
+          stepSize: 0,
+          currentStep: 0,
+          innerPosition: 0,
+          numberOfDots: 0,
+          slider: '',
+          isMouseDown:false,
+          startpointCoordinate: null,
+          isDistPositive: false,
+        }
+      },
+  
+      methods: {
+        calculateInnerPosition( decrease ) {
+          if ( decrease ) {
+            if ( this.optionsObject.loop && this.innerPosition <= 0 ) {
+              this.innerPosition = this.innerWidth - (this.slideWidth * this.optionsObject.itemsPerSlide);  
+            } else if ( this.innerPosition > 0 ){
+              this.innerPosition -= this.stepSize;
+            }
+          } else {
+            if(this.optionsObject.loop && this.innerPosition >=  this.innerWidth - (this.slideWidth * this.optionsObject.itemsPerSlide) ) {
+              this.innerPosition = 0;
+            }
+            else{
+              this.innerPosition += this.stepSize
+            }
+          }
+        },
+  
+        switchToNext() {
+          this.calculateInnerPosition( false );
+          this.$refs.inner.style.cssText = `transform: translateX(-${ this.innerPosition }px)`;
+          this.findandActivateCurrentStep()
+        },
+  
+        switchToPrev() {
+          this.calculateInnerPosition( true );
+          this.$refs.inner.style.cssText = `transform: translateX(-${ this.innerPosition }px)`;
+          this.findandActivateCurrentStep()
+        },
+  
+        switchToStep( step ) {
+          this.innerPosition = this.stepSize * ( step - 1);
+          this.$refs.inner.style.cssText = `transform: translateX(-${ this.innerPosition }px)`;
+          this.findandActivateCurrentStep()  
+        },
+  
+        findandActivateCurrentStep() {
+          if (this.optionsObject.dots) {
+            this.currentStep = this.innerPosition / this.stepSize + 1;
+            const dotButtons = document.querySelectorAll( `.carousel-dot` );
+            dotButtons.forEach(dot => dot.classList.remove( "active" ))
+            document.querySelector( `.carousel-dot[step="${ this.currentStep }"]` ).classList.add( "active" );
+          }
+        },
+  
+        startScroll() {
+          this.isMouseDown = false;
+          this.slider.classList.remove('active');
+          if(!this.isDistPositive) {
+            this.switchToNext()
+            this.findandActivateCurrentStep()
+          } else {
+            this.switchToPrev()
+            this.findandActivateCurrentStep()
+          }
+        },
+        mouseDown(e) {
+          this.isMouseDown = true;
+          this.slider.classList.add('active');
+          this.startpointCoordinate = e.pageX;
+        },
+        mouseMove(e) {
+          if(this.isMouseDown) {
+            e.preventDefault();
+            const endPointCoordinate = e.pageX;
+            const dist = (endPointCoordinate - this.startpointCoordinate);
+            if(dist < 0) {
+              this.isDistPositive = false
+            } else {
+              this.isDistPositive = true
+            }
+          }
+        }
+      },
+  
+      mounted() {
+  
+        // variables
+        const innerSelector = this.$refs.inner;
+        const slidesSelector = document.querySelectorAll( `.slides` );
+        const carouselWidth = innerSelector.clientWidth;
+  
+        // calculated variables
+        this.slideWidth = ( carouselWidth / this.optionsObject.itemsPerSlide );
+        this.stepSize = this.slideWidth * this.optionsObject.itemsPerStep;
+        this.numberOfDots = Math.ceil( ( slidesSelector.length - this.optionsObject.itemsPerSlide ) / this.optionsObject.itemsPerStep) + 1;
+        
+  
+        // add width for slides
+        for (let i = 0; i < slidesSelector.length; i++) {
+          slidesSelector[i].style.cssText = `min-width:${ this.slideWidth }px !important;`;
+        }
+  
+        
+        // set variables after adding slides width and margins
+        this.innerWidth = this.slideWidth * slidesSelector.length;
+  
+        // draggable slider
+        this.slider = document.querySelector('.inner');
+        this.slider.addEventListener('mousedown', this.mouseDown);
+        this.slider.addEventListener('mousemove', this.mouseMove);
+        this.slider.addEventListener('mouseup', this.startScroll);
+      },
+    }
+  </script>
+  
+  <style  scoped>
+  
+    .carousel {
+      position: relative;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding-bottom: 3rem;
+      margin: 50px 20px;
+  
+    }
+    .slider-wrapper {
+      width: 100%;
+      overflow: hidden;
+      margin: 50px 20px;
+    }
+  
+    .inner {
+      display: inline-flex;
+      align-items: baseline;
+      transition: 0.3s all ease;
+      width: 100%;
+    }
+    .inner.active {
+        cursor: grab;
+    }
+    .slides{
+      display: flex;
+      justify-content: center;
+    }
+  
+    .carousel-nav {
+      position: absolute;
+      display: flex;
+      justify-content: space-between;
+      width: 105%;
+    }
+    .testimonials-carousel{
+        width: 100%;
+        top: 45%;
+    }
+    svg {
+      fill: #594545;
+      width: 50px;
+    }
+  
+    button {
+      background-color: unset;
+      background-size: contain;
+      background-repeat: no-repeat;
+      border: none;
+      cursor: pointer;
+    }
+    .carousel-dots-wrapper {
+      width: 100%;
+      position: absolute;
+      bottom: 0;
+      z-index: 10;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .carousel-dot {
+      padding: 0;
+      width: 7px;
+      height: 7px;
+      border-radius: 15px;
+      background-color: #FFF8EA;
+      border: none;
+      margin: 3px;
+      cursor: pointer;
+      transition: 0.2s all linear;
+    }
+    .carousel-dot.active {
+      background-color: #594545;
+      width: 20px;
+    }
+    
+  </style>
--- cupcakeWeb/pages/index.vue
+++ cupcakeWeb/pages/index.vue
@@ -9,12 +9,49 @@
       </div>
       <img src="../assets/heroPic.png" alt="" width="518px" height="474px">
     </section>
+    <section class="carouselSection">
+      <h1>our most popular cupcakes</h1>
+      <Carousel :optionsObject="optionsObject">
+        <div class="slides" v-for="product in products" :key="product.id">
+          <NuxtLink :to="{name: 'blogs-id', params: {id: product.id } }" class="nuxtLink">
+            <div class="carouselDataContainer">
+              <img :src="link + product.imageUrl" alt="">
+              <h2>{{product.title}}</h2>
+            </div>
+          </NuxtLink>
+        </div>
+      </Carousel>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data() {
+    return {
+      optionsObject : {
+        // breakpoint: 1200,
+        itemsPerSlide: 3,
+        itemsPerStep: 2,
+        loop: true,
+        dots: true,
+        itemsMargin: null,
+        arrows: true
+      },
+      link: "http://localhost:1337",
+    }
+  },
+  computed: {
+      products(){
+          return this.$store.state.test.products.filter((product) => {
+            return product.id <= 6
+          });
+      }
+  },
+  async fetch () {
+      await this.$store.dispatch('test/getProductData')
+  },
 }
 </script>
 <style>
@@ -59,5 +96,48 @@ body {
 }
 .heroSection {
   margin-right: -60px;
+}
+.carouselSection {
+  margin: 88px -118px 0 -118px;
+  background: #9E7676;
+  padding: 112px 110px 112px 110px;
+  position: relative;
+}
+.carouselSection h1 {
+  font-size: 34px;
+  line-height: 51px;
+  text-align: center;
+  letter-spacing: 0.29em;
+  color: #FFF8EA;
+  margin: 0;
+}
+.carouselSection::before, .carouselSection::after {
+  clip-path: ellipse(50% 49% at 50% 50%);
+  background-color: #FFF8EA;
+  height: 114px;
+  width: 1440px;
+  content: '';
+  position: absolute;
+}
+.carouselSection::before {
+  left: 0;
+  top: -57px;
+}
+.carouselSection::after {
+  bottom: -57px;
+  left: 0;
+}
+.nuxtLink {
+  text-decoration: none;
+  color: #FFF8EA;
+  text-align: center;
+}
+.carouselDataContainer img {
+  height: 206px;
+  width: 206px;
+  border-radius: 50%;
+}
+.carouselDataContainer h2 {
+  font-size: 18px;
 }
 </style>
