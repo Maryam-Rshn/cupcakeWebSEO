6944754146e4369baf2ca54dfac46e4d3957563f
Maryam_rshn <mari.r9777@gmail.com>
2022-12-20 15:49:59 +0330

    services section added and styled

--- cupcakeWeb/pages/index.vue
+++ cupcakeWeb/pages/index.vue
@@ -22,6 +22,40 @@
         </div>
       </Carousel>
     </section>
+    <section class="servicesSection">
+      <h2>services</h2>
+      <h1>Quality & Passion with our Service!</h1>
+      <div class="servicesContainer">
+        <div class="service">
+          <div class="iconContainer">
+            <img class="pastryIcon" src="../assets/pastryBag.png" alt="">
+          </div>
+          <p>Best cupcake decorating ideas</p>
+          <button>Read More</button>
+        </div>
+        <div class="service">
+          <div class="iconContainer">
+            <img class="icon" src="../assets/iconmonstr-time-19-240.png" alt="">
+          </div>
+          <p>Quick and Easy  Cupcake Recipe</p>
+          <button>Read More</button>
+        </div>
+        <div class="service">
+          <div class="iconContainer">
+            <img class="icon" src="../assets/iconmonstr-shopping-cart-thin-240.png" alt="">
+          </div>
+          <p>Using Ingredients that are Easy to Access</p>
+          <button>Read More</button>
+        </div>
+        <div class="service">
+          <div class="iconContainer">
+            <img class="icon" src="../assets/iconmonstr-medal-6-240.png" alt="">
+          </div>
+          <p>Award winning cupcake recipes</p>
+          <button>Read More</button>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -139,5 +173,73 @@ body {
 }
 .carouselDataContainer h2 {
   font-size: 18px;
+}
+.servicesSection {
+  margin: 78px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.servicesSection h2 {
+  font-size: 19px;
+  letter-spacing: 0.29em;
+  color: #594545;
+  margin: 0;
+}
+.servicesSection h1 {
+  font-size: 45px;
+  text-align: center;
+  color: #594545;
+  width: 635px;
+  margin: 20px 0;
+}
+.servicesContainer {
+  margin-top: 49px;
+  display: flex;
+  gap: 30px;
+}
+.service {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.iconContainer {
+  height: 160px;
+  width: 160px;
+  position: relative;
+  background-image: url(../assets/servicesBackground.png);
+  background-repeat: no-repeat;
+  background-size: contain;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.icon {
+  width: 60px;
+  height: 60px;
+  margin-top: 40px;
+}
+.pastryIcon {
+  width: 76px;
+  height: 76px;
+  margin-top: 20px;
+}
+.service p {
+  margin: 65px 0 20px 0;
+  font-size: 22px;
+  line-height: 45px;
+  text-align: center;
+  color: #594545;
+  width: 260px;
+}
+.service button {
+  background-color: #9E76763B;
+  border-color: transparent;
+  padding: 12px 40px;
+  border-radius: 26px;
+  text-align: center;
+  font-size: 20px;
+  font-weight: bold;
+  color: #594545;
 }
 </style>
