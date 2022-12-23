1268f9c1075e6aed5e7ab86ff84d978bd219b0ac
Maryam_rshn <mari.r9777@gmail.com>
2022-12-23 22:43:24 +0330

    Hero section added to about us page and styled

--- cupcakeWeb/pages/abouts/index.vue
+++ cupcakeWeb/pages/abouts/index.vue
@@ -1,6 +1,31 @@
 <template>
   <div class="container">
     <NavBar />
+    <section class="aboutusHeroSection">
+        <div class="ourStoryContainer">
+            <img src="../../assets/About.png" alt="">
+            <div class="ourStoryContent">
+                <h1 class="transparentTitle">About</h1>
+                <h2>Our Story</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet, consectetur  Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet,</p>
+            </div>
+        </div>
+        <div class="contactUsContainer">
+            <img src="../../assets/contact.png" alt="">
+            <div class="contactUsContent">
+                <h1 class="transparentTitle">contact</h1>
+                <h2>Reach us here</h2>
+                <p><span>Email:</span>cup@gamil.com</p>
+                <p><span>Phone:</span>+989127345780</p>
+                <div class="icons">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none"><g clip-path="url(#clip0_151_21)"><path d="M24 0C10.746 0 0 10.746 0 24C0 37.254 10.746 48 24 48C37.254 48 48 37.254 48 24C48 10.746 37.254 0 24 0ZM20 32H16V20H20V32ZM18 18.218C16.786 18.218 15.8 17.226 15.8 16C15.8 14.776 16.784 13.782 18 13.782C19.216 13.782 20.2 14.776 20.2 16C20.2 17.226 19.214 18.218 18 18.218ZM34 32H30.004V26.278C30.004 22.516 26 22.834 26 26.278V32H22V20H26V22.186C27.744 18.954 34 18.714 34 25.282V32Z" fill="#9E7676"/></g><defs><clipPath id="clip0_151_21"><rect width="48" height="48" fill="white"/></clipPath></defs></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none"><g clip-path="url(#clip0_151_23)"><path d="M24 0C10.746 0 0 10.746 0 24C0 37.254 10.746 48 24 48C37.254 48 48 37.254 48 24C48 10.746 37.254 0 24 0ZM36.132 19.29C36.498 27.37 30.472 36.378 19.804 36.378C16.56 36.378 13.542 35.426 11 33.796C14.048 34.156 17.09 33.308 19.504 31.418C16.992 31.372 14.87 29.71 14.136 27.428C15.038 27.6 15.926 27.55 16.732 27.33C13.97 26.774 12.062 24.286 12.124 21.624C12.9 22.054 13.784 22.312 14.726 22.342C12.168 20.632 11.444 17.254 12.948 14.672C15.78 18.148 20.014 20.434 24.788 20.674C23.95 17.082 26.676 13.62 30.386 13.62C32.036 13.62 33.53 14.318 34.578 15.434C35.886 15.178 37.118 14.698 38.226 14.04C37.796 15.382 36.886 16.506 35.7 17.218C36.862 17.078 37.97 16.77 38.998 16.312C38.23 17.468 37.258 18.48 36.132 19.29V19.29Z" fill="#9E7676"/></g><defs><clipPath id="clip0_151_23"><rect width="48" height="48" fill="white"/></clipPath></defs></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none"><g clip-path="url(#clip0_151_25)"><path d="M29.658 12.604C28.182 12.536 27.738 12.524 24 12.524C20.262 12.524 19.82 12.538 18.344 12.604C14.546 12.778 12.778 14.576 12.604 18.344C12.538 19.82 12.522 20.262 12.522 24C12.522 27.738 12.538 28.18 12.604 29.658C12.778 33.416 14.538 35.224 18.344 35.398C19.818 35.464 20.262 35.48 24 35.48C27.74 35.48 28.182 35.466 29.658 35.398C33.456 35.226 35.222 33.422 35.398 29.658C35.464 28.182 35.478 27.738 35.478 24C35.478 20.262 35.464 19.82 35.398 18.344C35.222 14.578 33.452 12.778 29.658 12.604V12.604ZM24 31.19C20.03 31.19 16.81 27.972 16.81 24C16.81 20.03 20.03 16.812 24 16.812C27.97 16.812 31.19 20.03 31.19 24C31.19 27.97 27.97 31.19 24 31.19ZM31.474 18.208C30.546 18.208 29.794 17.456 29.794 16.528C29.794 15.6 30.546 14.848 31.474 14.848C32.402 14.848 33.154 15.6 33.154 16.528C33.154 17.454 32.402 18.208 31.474 18.208V18.208ZM28.666 24C28.666 26.578 26.576 28.666 24 28.666C21.424 28.666 19.334 26.578 19.334 24C19.334 21.422 21.424 19.334 24 19.334C26.576 19.334 28.666 21.422 28.666 24ZM24 0C10.746 0 0 10.746 0 24C0 37.254 10.746 48 24 48C37.254 48 48 37.254 48 24C48 10.746 37.254 0 24 0ZM37.916 29.772C37.686 34.862 34.852 37.682 29.774 37.916C28.28 37.984 27.802 38 24 38C20.198 38 19.722 37.984 18.228 37.916C13.14 37.682 10.318 34.858 10.084 29.772C10.016 28.28 10 27.802 10 24C10 20.198 10.016 19.722 10.084 18.228C10.318 13.14 13.142 10.318 18.228 10.086C19.722 10.016 20.198 10 24 10C27.802 10 28.28 10.016 29.774 10.086C34.864 10.32 37.688 13.15 37.916 18.228C37.984 19.722 38 20.198 38 24C38 27.802 37.984 28.28 37.916 29.772Z" fill="#9E7676"/></g><defs><clipPath id="clip0_151_25"><rect width="48" height="48" fill="white"/></clipPath></defs></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none"><g clip-path="url(#clip0_151_27)"><path d="M24 0C10.746 0 0 10.746 0 24C0 37.254 10.746 48 24 48C37.254 48 48 37.254 48 24C48 10.746 37.254 0 24 0ZM30 16H27.3C26.224 16 26 16.442 26 17.556V20H30L29.582 24H26V38H20V24H16V20H20V15.384C20 11.846 21.862 10 26.058 10H30V16Z" fill="#9E7676"/></g><defs><clipPath id="clip0_151_27"><rect width="48" height="48" fill="white"/></clipPath></defs></svg>
+                </div>
+            </div>
+        </div>
+    </section>
   </div>
 </template>
 
@@ -11,5 +36,69 @@ export default {
 </script>
 
 <style>
-
+.aboutusHeroSection {
+    margin: 58px -60px 0 -60px;
+    padding: 57px 60px 160px 60px;
+    background-color: rgba(158, 118, 118, 0.21);
+    border-radius: 33px;
+    box-shadow: 6px 6px 4px rgba(0, 0, 0, 0.25);
+}
+.ourStoryContainer, .contactUsContainer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.ourStoryContent, .contactUsContent {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+}
+.contactUsContainer {
+    flex-direction: row-reverse;
+    justify-content: end;
+    gap: 320px;
+    margin-top: 109px;
+}
+.ourStoryContainer img {
+    height: 586px;
+    margin-left: -60px;
+}
+.aboutusHeroSection h1, .ourTeamHeader h1 {
+    color: rgba(158, 118, 118, 0.31);
+    font-size: 110px;
+    letter-spacing: 0.29em;
+    position: absolute;
+    top: 0;
+    margin: 0;
+}
+.aboutusHeroSection h2, .ourTeamHeader h2 {
+    color: #594545;
+    letter-spacing: 0.03em;
+    font-size: 49px;
+    margin: 70px 0 40px 0;
+    z-index: 1;
+}
+.ourStoryContent p {
+    font-size: 20px;
+    color: #594545;
+    line-height: 30px;
+    width: 429px;
+}
+.contactUsContainer p {
+    font-size: 20px;
+    color: #594545;
+    margin: 0 0 20px 0;
+    font-weight: 100;
+}
+.contactUsContainer span {
+    font-weight: 1000;
+    margin-right: 10px;
+    font-size: 22px;
+}
+.icons {
+    display: flex;
+    margin-top: 8px;
+    gap: 20px;
+}
 </style>
