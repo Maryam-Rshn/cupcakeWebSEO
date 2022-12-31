0d9726c65e796862e8246a2d191f97aaefbfd43d
Maryam_rshn <mari.r9777@gmail.com>
2022-12-31 16:02:52 +0330

    test

--- cupcakeWeb/pages/index.vue
+++ cupcakeWeb/pages/index.vue
@@ -2,6 +2,7 @@
   <div class="container">
     <NavBar />
     <section class="heroSection">
+      hi
       <div>
         <h2>the ultimate cupcake baking guide</h2>
         <h1>Make people smile, with <span>cupcakes.</span></h1>
